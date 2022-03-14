01b928ced896dfca9bf6b88a471cc31e726926a9
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-03-15 01:05:21 +0600

    Kaden's Algorithm

---
+++ Kaden's-Algorithms.java
@@ -0,0 +1,20 @@
+/**
+ *if current sum is negative then assign current sum = 0
+ */
+public class KadensAlgorithm {
+    
+    public static int maxSumArray(int a[]){
+        int max_sum = 0, cur_sum = 0;
+        for(int i = 0; i < a.length; i++){
+            cur_sum = cur_sum + a[i];
+            if(cur_sum > max_sum)max_sum = cur_sum;
+            if(cur_sum < 0) cur_sum = 0;
+        }
+        return max_sum;
+    }
+    
+    public static void main(String[] args) {
+        int a[] = {5, -4, -2, 6, -1};
+        System.out.print(maxSumArray(a));
+    }
+}
