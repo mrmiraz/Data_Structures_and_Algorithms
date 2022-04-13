03a1390367f2599af9db6e2df85296ce78011a22
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-04-13 22:07:39 +0600

    Sieve of Eratosthenes

---
+++ Number_Theory/SeiveAlgorithm.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class SeiveAlgorithm {
+    
+    /*
+    Sieve of Eratosthenes Algorithms for finding the prime numbers
+    this function will return a boolean array which represents that 
+    which of the number(number is represented by the the array index)
+    is prime or not in the range of 1 to n
+    
+    Algorithm:
+    Step 1: take a boolean array of size n+1 and fill with true value
+    Step 2: run a loop from 2 to sqrt(n)
+    Step 3: fill all the product of i with false value except i
+    */
+  
+    public static boolean[] seive(int n){
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for(int i = 2; i*i < n; i++){
+            for(int j = 2*i; j <= n; j+=i){
+                isPrime[j] = false;
+            }
+        }
+        return isPrime;
+    }
+    
+    public static void main(String[] args) {
+        boolean[] prime = seive(100);
+        for(int i =0;i < prime.length; i++){
+            if(prime[i])System.out.print(i + " ");
+        }
+        
+    }
+}
