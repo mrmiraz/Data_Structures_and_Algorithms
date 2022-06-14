60abe40a9a9fb6acb979b7134d51524f1bf16744
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-06-15 00:39:37 +0600

    Count number of ways to cover a distance

---
+++ Dynamic-Programming/CoverDistance.java
@@ -0,0 +1,47 @@
+/*
+Problem:Given a distance ‘dist’, count total number of ways to cover the distance with 1, 2 and 3 steps. 
+
+Algorithm: 
+Step 1: Create a recursive function (count(int n)) which takes only one parameter.
+Step 2: Check the base cases. If the value of n is less than 0 then return 0, and if value of n is equal to zero then return 1 as it is the starting position.
+Step 3: Call the function recursively with values n-1, n-2 and n-3 and sum up the values that are returned, i.e. sum = count(n-1) + count(n-2) + count(n-3).
+Step 4: Return the value of sum.
+
+Time Complexity: O(N)
+
+Sample input:
+4
+Sample output:
+7
+*/
+
+public class CoverDistance {
+    
+    //recursion solution
+    //O(3^n): O(1)
+    public static int printCountRec(int dist){
+        if(dist == 0)return 1;
+        if(dist < 0) return 0;
+        
+        return printCountRec(dist-1)+
+                printCountRec(dist-2)+
+                printCountRec(dist-3);
+    }
+    
+    //top down approch //O(N):O(N)
+    public static int printCountdp(int dist){
+        int[] dp = new int[dist+1];
+        dp[0] = 1;
+        if(dist >= 1)dp[1] = 1;
+        if(dist >= 2) dp[2] = 2;
+        for(int i = 3; i <= dist; i++){
+            dp[i] = dp[i-1] + dp[i-2]+ dp[i-3];
+        }
+        return dp[dist];
+    }
+    
+    public static void main(String[] args) {
+        int dist = 4;
+        System.out.println(printCountdp(dist));
+    }
+}
